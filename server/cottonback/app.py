e250471602d8f6f953d7195d05e08e8bb987ec90
Luka Stojanovic <luka.stojanovic@sbgenomics.com>
2015-08-04 15:00:11 +0200

    copypasta

--- server/cottonback/app.py
+++ server/cottonback/app.py
@@ -1,9 +1,17 @@
-from flask import Flask, jsonify, send_file
 import os
+from flask import Flask, jsonify, send_file
+from flask_mongokit import MongoKit
+from social.apps.flask_app.routes import social_auth
+
+from cottonback.db import *
 
 app = Flask("cottonback", static_url_path="", static_folder=os.path.abspath("client/.tmp"))
+app.register_blueprint(social_auth)
+SOCIAL_AUTH_USER_MODEL = 'cottonback.db.User'
 
-print os.path.abspath("client/src")
+db = MongoKit(app)
+db.register([User])
+
 
 @app.route('/')
 def index():
