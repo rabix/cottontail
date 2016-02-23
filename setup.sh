5e53a3e98360ab42886889d81c2aedd9b11f8f85
Filip Jelic <jelic.filip@gmail.com>
2016-02-24 00:46:01 +0100

    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
-node post-install.js
-
 cd client
 npm install # postInstall will do `bower install`
-gulp build
+gulp build && gulp dev
+
+cd src
+git clone git@github.com:rabix/cottontail-editors.git editors
+cd editors
+npm install # postInstall will do `bower install`
+grunt templates && grunt sass
+
+cd ../../../ # cottontail root
 
 echo "Finished building cottontail."
+
+echo ""
+echo "----"
+echo "Run '(sudo) npm link' to get cottontail in path"
+echo "----"
+echo ""
+
 echo "Run 'cottontail setup' to setup cottontail env."
+
