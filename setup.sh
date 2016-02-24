4a99538c68fbcca7ff19ebf51e7580112eff92b9
Mate Balo Bordas <mate.balobordas@sbgenomics.com>
2016-02-24 15:43:59 +0100

    Fix(cottontail): Fixed badly merged setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-npm install
 cd client
-npm install
-cd src/editors
-npm install
-grunt sass
-grunt templates
+npm install # postInstall will do `bower install`
+gulp build && gulp dev
+
+cd src
+git clone git@github.com:rabix/cottontail-editors.git editors
+cd editors
+npm install # postInstall will do `bower install`
+grunt templates && grunt sass
+
+cd ../../../ # cottontail root
+
+echo "Finished building cottontail."
+
+echo ""
+echo "----"
+echo "Run '(sudo) npm link' to get cottontail in path"
+echo "----"
+echo ""
+
+echo "Run 'cottontail setup' to setup cottontail env."
+
