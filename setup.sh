470d2e29966d2bf65effe646241515686307cc9c
Filip Jelic <jelic.filip@gmail.com>
2016-02-25 11:57:28 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -2,15 +2,19 @@
 
 cd client
 npm install # postInstall will do `bower install`
-gulp build && gulp dev
 
 cd src
 git clone git@github.com:rabix/cottontail-editors.git editors
 cd editors
 npm install # postInstall will do `bower install`
-grunt templates && grunt sass
+grunt templates && grunt sass && grunt build
 
 cd ../../../ # cottontail root
+
+cd client/
+gulp build && gulp dev
+cd ..
+
 
 echo "Finished building cottontail."
 
