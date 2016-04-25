8f25959d2e02328b8a62a34480d49e526269a778
Ivan Batić <ivan.batic@live.com>
2016-04-25 10:31:33 +0200

    Scaffolded the NG2 version

--- post-install.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by filip on 8/27/15.
- */
-var config = require('./cli/config');
-
-config.create({}, '.');
--- server/api/common/common.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-let config = require('../../config/environment/index');
-
-exports.getConfig = function (req, res) {
-    return res.json(config);
-};
