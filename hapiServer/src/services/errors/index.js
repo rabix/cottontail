556bc0bb0ad75ea3a85dcb4d4ba834f73554b357
Mate Balo Bordas <mate.balobordas@sbgenomics.com>
2016-05-05 16:07:58 +0200

    removed unused function from errors service

--- hapiServer/src/services/errors/index.js
+++ hapiServer/src/services/errors/index.js
@@ -6,24 +6,6 @@
 var logger = require('../logger');
 
 module.exports = {
-    '404': function pageNotFound(req, res) {
-        var viewFilePath = '404';
-        var statusCode = 404;
-        var result = {
-            status: statusCode
-        };
-
-        res.status(result.status);
-        //res.render(viewFilePath, function (err) {
-        //    if (err) {
-        //        return res.json(result, result.status);
-        //    }
-        //
-        //    res.render(viewFilePath);
-        //});
-
-        res.json({message: 'Not found.'})
-    },
     handle: function (err) {
         if (err.code === 'EACCES') {
             logger.error('Seems like we don\'t have write permissions here. ', err);
