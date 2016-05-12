0b68b127ee0eb32a083bcc410c81cd649dbb2072
Ivan Batić <ivan.batic@live.com>
2016-05-12 13:56:02 +0200

    Fixed incorrect NodeJS version assertion.

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ try {
     'use strict';
 
     var Hapi = require('hapi');
-    
+
     process.env.NODE_ENV = process.env.NODE_ENV || 'development';
     var validationComponent = require('./src/services/validation/validation.service');
 
@@ -45,7 +45,7 @@ try {
             Logger.info('Working dir exists.');
         });
     }
-    
+
     /**
      * Create a server with a host and port */
     var server = new Hapi.Server();
@@ -54,9 +54,9 @@ try {
         host: config.host,
         port: config.port
     });
-    
+
     /* Register the  plugins */
-    server.register(plugins, function(err) {
+    server.register(plugins, function (err) {
         if (err) {
             console.log('Failed loading the plugin: ' + err);
         }
@@ -71,7 +71,7 @@ try {
                 response.header('Access-Control-Allow-Origin', '*');
                 response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Authorization, Content-Type, Accept');
                 response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-                
+
                 reply.continue();
             }
 
@@ -90,7 +90,7 @@ try {
             }
         });
 
-        server.on('internalerror', function(req, error) {
+        server.on('internalerror', function (req, error) {
             console.log('error', error);
         });
 
@@ -117,17 +117,29 @@ try {
     // it's most likely the result of a NodeJS version being lower than 4
     if (exception.name === 'SyntaxError') {
 
-        // v4 is when the ES6 came in, 4.2 is the LTS
+        var versionIsSatisfied = true;
         var versionRequirement = [4, 2, 0];
+        var processVersion = process.version.slice(1).split('.').map(function (num) {
+            return ~~num
+        });
 
-        var versionIsSatisfied = process.version.slice(1).split('.').reduce(function (prev, current, i) {
-            return prev === true && ~~current >= versionRequirement[i];
-        }, true);
+
+        for (var i = 0; i < 3; i++) {
+            if (processVersion[i] > versionRequirement[i]) {
+                versionIsSatisfied = true;
+                break;
+            } else if (processVersion[i] < versionRequirement[i]) {
+                versionIsSatisfied = false;
+                break;
+            }
+        }
 
         if (!versionIsSatisfied) {
             console.warn('Hey there.\nWe have encountered an error that\'s most likely ' +
                 'caused by an outdated NodeJS version (%s).' +
-                '\nPlease consider updating to at least v%s.\n\nThanks,\nThe Rabix Team',
+                '\nPlease consider updating to at least v%s.\n\nThanks,\nThe Rabix Team' +
+                '\n\nP.S. Here\'s the error: ' + exception
+                ,
                 process.version,
                 versionRequirement.join('.'));
         } else {
